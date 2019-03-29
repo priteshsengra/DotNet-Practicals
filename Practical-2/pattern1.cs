6197a811186a76b14a87e211425506f1494434f2
priteshsengra <46516055+priteshsengra@users.noreply.github.com>
2019-03-29 21:10:01 +0530

    Practical-2

---
+++ Practical-2/pattern1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Pattern
+{
+    class PatternExample
+    {
+        public static void Main()
+        {
+            int i,j;
+            for (j = 5; j > 0; j--)
+            {
+                for (i = j; i > 0; i--)
+                    Console.Write("@ ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
